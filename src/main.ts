0129e2f977150565fc7449750bf25b65bc5a5f6a
Alexander <subbsan@gmail.com>
2023-02-20 20:07:10 +0300

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston/dist/winston.constants';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+
+  app.use(helmet()); // защита от XSS-атак
+  app.use(csurf()); // защита от CSRF-атак
 
   await app.listen(3000);
 }
---
+++ src/users/users.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { Users } from './users';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+
+@Module({
+  providers: [Users, UsersService],
+  controllers: [UsersController]
+})
+export class UsersModule {}
